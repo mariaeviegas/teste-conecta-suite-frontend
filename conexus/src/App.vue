9c1623d05d2aa124cdab2ca2233e9cc8ad7d4156
mariaeviegas <maria.viegas@sptech.school>
2025-05-10 21:01:54 -0300

    Add: Configuração inicial do projeto com vuetify

---
+++ conexus/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app>
+    <RouterView />
+  </v-app>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  
+}
+</style>
