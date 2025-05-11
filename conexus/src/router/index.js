f574f7266ba3f5d3261c5e62da3efc91c4e59f48
mariaeviegas <maria.viegas@sptech.school>
2025-05-11 15:59:21 -0300

    Layout da tela de cadastro

--- conexus/src/router/index.js
+++ conexus/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import PlanManagementView from '../views/PlanManagementView.vue'
 import Login from '../views/LoginView.vue'
+import Register from '../views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/cadastro',
+      name: 'Register',
+      component: Register
     },
   ]
 })
